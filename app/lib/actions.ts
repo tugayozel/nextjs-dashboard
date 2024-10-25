45f58d5ccaced231cff67b82321da8d91a7e1d5d
tugayozel <ozel_tugay@hotmail.com>
2024-10-25 18:05:22 +0200

    third commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,6 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
+        console.log(error)
         // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.'
@@ -95,6 +96,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
             WHERE id = ${id}
         `;   
     } catch (error) {
+        console.log(error)
         return {
             message: 'Database Error: Failed to Update Invoice.'
         };
@@ -112,6 +114,7 @@ export async function deleteInvoice(id: string) {
             message: 'Deleted Invoice.',
         };
     } catch (error) {
+        console.log(error)
         return {
             message: 'Database Error: Failed to Delete Invoice.'
         };
